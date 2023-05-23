af04f683f659df7fd0d6fc3eb94d643196d05802
Charles Chou <charles_chou@accton.com>
2023-05-23 01:55:08 +0000

    Add virtualization group - Define VirtualMachine spec

---
+++ staging/src/kubesphere.io/api/virtualization/v1alpha1/doc.go
@@ -0,0 +1,8 @@
+// Package v1alpha1 contains API Schema definitions for the virtualization v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=kubesphere.io/api/virtualization
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=virtualization.ecpaas.io
+
+package v1alpha1
